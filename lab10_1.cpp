ddc220c550354b47315b8ae5516f5f29499928b6
ChonlananT <ciwerxil@gmail.com>
2021-12-14 16:31:02 +0700

    lab10_1.cpp completed

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -3,9 +3,13 @@
 using namespace std;
 
 int main(){	
+	double loan, intrate, pay;
 	cout << "Enter initial loan: ";
+	cin >> loan;
 	cout << "Enter interest rate per year (%): ";
+	cin >> intrate;
 	cout << "Enter amount you can pay per year: ";
+	cin >> pay;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -20,14 +24,34 @@ int main(){
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
 	
+	for(int i = 1;true; i++){
+	int year = i;
+	double interest = loan*(intrate/100.0);
+	double total = loan + interest;
+	double newb = total - pay;
+	if(total < pay){
+		pay = total;
+	}
+	
+	cout << fixed << setprecision(2); 
+	cout << setw(13) << left << year; 
+	cout << setw(13) << left << loan;
+	cout << setw(13) << left << interest;
+	cout << setw(13) << left << total;
+	cout << setw(13) << left << pay;
+	if(newb > 0){
+		newb = newb;
+		cout << setw(13) << left << newb;
+	}if(newb < 0){
+		newb = 0;
+		cout << setw(13) << left << newb;
+		break;
+	}
+	
+	cout << "\n";
+	loan = newb;
+	
+	}
 	return 0;
 }
